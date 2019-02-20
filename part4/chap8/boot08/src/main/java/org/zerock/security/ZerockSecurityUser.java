b9c97bb53a7371b50f688a46e6aaa1876104c6e5
LeeInHaeng <lih0420@naver.com>
2019-02-20 22:06:09 +0900

    noop delete

--- part4/chap8/boot08/src/main/java/org/zerock/security/ZerockSecurityUser.java
+++ part4/chap8/boot08/src/main/java/org/zerock/security/ZerockSecurityUser.java
@@ -17,7 +17,7 @@ public class ZerockSecurityUser extends User {
 	
 	public ZerockSecurityUser(Member member) {
 		
-		super(member.getUid(), "{noop}"+member.getUpw(), makeGrantedAuthority(member.getRoles()));
+		super(member.getUid(), member.getUpw(), makeGrantedAuthority(member.getRoles()));
 		
 		this.member = member;
 	}
